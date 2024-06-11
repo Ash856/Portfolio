ba51d7b652babea2fa9b60e4d0b43abf1ae4381a
Ashish Kumar <raiashish856@gmail.com>
2024-06-11 12:22:50 +0530

    Add files via upload
    Initial commit , major project work ,changes to be made - refactor ,add features ,remove things and readme update

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function toggleMobileMenu(){
+    document.getElementById("menu").classList.toggle("active");
+}
+
+
+const toggle_btn = document.querySelector('#checkbox')
+
+toggle_btn.addEventListener('change',() => {
+
+
+if(toggle_btn.checked){
+    document.body.classList.add('dark-mode')
+}else{
+    document.body.classList.remove('dark-mode')
+}
+})
